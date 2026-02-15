0989f06fc41905fce1e119ec6cca4bf9623f06cf
exeKor <yavar.korkian@gmail.com>
2026-02-14 23:02:13 -0500

    load

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,12 @@
+
 from the_well.data import WellDataset
 from torch.utils.data import DataLoader
 
 trainset = WellDataset(
     well_base_path="./data/datasets/",
     well_dataset_name="active_matter",
-    well_split_name="train"
-)
+    well_split_name="train")
 
 train_loader = DataLoader(trainset)
+
+trainset.__getitem__(train_loader)
