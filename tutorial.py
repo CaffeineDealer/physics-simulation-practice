6024aa7fbb3b79bbd8c854d48b768ea2a4e964f5
exeKor <yavar.korkian@gmail.com>
2026-02-15 21:32:22 -0500

    plot added

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,9 @@
-
+#%%
 from the_well.data import WellDataset
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from IPython import display
+import numpy as np
 
 trainset = WellDataset(
     well_base_path="./data/datasets/",
@@ -9,4 +12,21 @@ trainset = WellDataset(
 
 train_loader = DataLoader(trainset)
 
-trainset.__getitem__(train_loader)
+#%%
+batch = next(iter(train_loader))
+print(batch.keys())
+data = batch['input_fields']
+# %%
+data = np.squeeze(data)
+n1, n2, n3 = data.shape
+
+fig, ax = plt.subplots(figsize=(15, 8))
+
+for i in range(n3):
+    ax.clear()
+    ax.imshow(data[:, :,i])
+    fig.canvas.draw()
+    display.display(fig)  
+    display.clear_output(wait=True)
+    plt.pause(0.1)  
+plt.show()
